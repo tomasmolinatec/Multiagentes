19e955e1e9f965c1b7d0074c7f9ebf7d79abbb8d
Diego Abdo <diegoabdov@gmail.com>
2024-11-27 12:49:14 -0600

    secret weapon 2

--- model.py
+++ model.py
@@ -157,9 +157,9 @@ class CityModel(Model):
         for pos, dir in self.TLDirections.items():
             TL = self.grid.get_cell_list_contents([pos])[0]
             TL.direction = dir
-            agent = Road( (pos[0], pos[1]), self, dir)
+            agent = Road(self.unique_id, self, (pos[0], pos[1]), dir)
+            self.unique_id += 1
             self.grid.place_agent(agent, (pos[0], pos[1]))
-
 
         self.running = True
 
